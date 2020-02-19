0652270f00bbc4c8a511f1fcec452bd0e7ba9f56
botheaj <botheaj@ucmail.uc.edu>
2020-02-18 19:23:13 -0500

    lab 6 walkthru

--- node/server.js
+++ node/server.js
@@ -1,8 +1,43 @@
 var http = require("http"); 
+var fs = require("fs");
+var os = require("os");
+var ip = require("ip");
+
 
 var server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type":"text/html"}); 
-    res.end("Hello from Node!");
+    if(req.url === "/") {
+        fs.readFile("./public/index.html","UTF-8",function(err, body){
+            res.writeHead(200, {"Content-Type":"text/html"}); 
+            res.end(body);
+        });
+    }
+
+    else if(req.url.match("/sysinfo")){
+        myHostName=os.hostname();
+        html=`
+        <!DOCTYPE html>
+        <html>
+         <head> 
+         <title> System Info </title>
+         </head>
+         <body>
+         <p>Hostname: ${myHostName}</p>
+         <p>IP: ${ip.address()}</p>
+         <p>Server Uptime: </p>
+         <p>Total Memory: </p>
+         <p>Free Memory: </p>
+         <p>Number of CPUs: </p>
+         </body>
+         </html>
+        `
+        res.writeHead(200, {"Content-Type":"text/html"});
+        res.end(html);
+    }
+
+    else {
+        res.writeHead(404, {"Content-Type":"text/plain"});
+        res.end(`404 File Not Found at ${req.url}`); 
+    }
 });
 
 server.listen(3000); 
